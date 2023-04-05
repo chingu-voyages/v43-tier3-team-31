03b7f782ed402c11e523bf32351013f9e32e0e7d
vikraj01 <99103720+vikraj01@users.noreply.github.com>
2023-04-05 15:10:55 +0530

    feat: add my post section

---
+++ dina-frontend/pages/dashboard/my-post/empty-posts.tsx
@@ -0,0 +1,12 @@
+import DashboardLayout from '@/components/layouts/DashboardLayout'
+import React from 'react'
+
+const EmptyPosts = () => {
+  return (
+    <DashboardLayout>
+        
+    </DashboardLayout>
+  )
+}
+
+export default EmptyPosts
