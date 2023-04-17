07444d7d6d5a4528de9aa6fa12730c8793df7c9b
Mocarram Hossain <mocarramhossain@gmail.com>
2023-04-17 06:53:11 +0600

    feat: functional all posts page

--- dina-frontend/pages/all-posts.tsx
+++ dina-frontend/pages/all-posts.tsx
@@ -1,109 +1,82 @@
-import Image from 'next/image'
-import Logo from '../public/logo.png'
-
+import Image from "next/image";
+import Logo from "../public/logo.png";
+import React, { useState, useEffect } from "react";
+import {
+  getFirestore,
+  collection,
+  query,
+  orderBy,
+  onSnapshot,
+} from "firebase/firestore";
+import { db } from "../utils/firebaseClient";
+import Navbar from "@/components/publicPages/landing/Navbar";
+import Link from "next/link";
 
 const AllPost = () => {
-    return (
-        <>
-            <nav className="flex px-16 py-6 items-center">
-                <div className='flex items-center'>
-                    <Image className='inline-block ml-2 w-12 h-12 mr-2' src={Logo} alt='DiNA' width="50" height="50"></Image>
-                    <span className='text-lg font-bold text-gray-700'>DiNA</span>
-                </div>
-                <div className='ml-4 flex-1'>
-                    <form className="flex w-96 items-center">
-                        <label htmlFor="simple-search" className="sr-only">Search</label>
-                        <div className="relative w-full">
-                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
-                            </div>
-                            <input type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for food..." required />
-                        </div>
-                    </form>
-                </div>
+  const [posts, setPosts] = useState([]);
 
-                <ul className='flex gap-3 items-center'>
-                    <li>
-                        Login
-                    </li>
-                    <li>About</li>
-                    <li>
-                        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Get Started</button>
-                    </li>
-                </ul>
-            </nav>
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const postsRef = collection(db, "posts");
+      const q = query(postsRef, orderBy("timestamp", "desc"));
 
+      const unsubscribe = onSnapshot(q, (snapshot) => {
+        const fetchedPosts = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setPosts(fetchedPosts);
+      });
 
-            <div className='flex px-6'>
-                <div className='border-r border-gray pt-8 px-6'> {/*Sidebar*/}
-                    <div>All posts</div>
-                    {/* Just a fix for now */}
-                    <div className='mr-4'>Ingredient Glossary</div>
+      return () => unsubscribe();
+    };
 
-                </div>
-                <div className='flex-1 px-12 mb-16'> {/* Main Content 1. ... 2. ...*/}
-                    <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-                        <div className="grid gap-4">
-                            <div>
-                                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt="" />
-                            </div>
-                            <div>
-                                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt="" />
-                            </div>
-                            <div>
-                                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt="" />
-                            </div>
-                        </div>
-                        <div className="grid gap-4">
-                            <div>
-                                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="" />
-                            </div>
-                            <div>
-                                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt="" />
-                            </div>
-                            <div>
-                                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt="" />
-                            </div>
-                        </div>
-                        <div className="grid gap-4">
-                            <div>
-                                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" alt="" />
-                            </div>
-                            <div>
-                                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt="" />
-                            </div>
-                            <div>
-                                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt="" />
-                            </div>
-                        </div>
-                        <div className="grid gap-4">
-                            <div>
-                                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" alt="" />
-                            </div>
-                            <div>
-                                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" alt="" />
-                            </div>
-                            <div>
-                                <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" alt="" />
-                            </div>
-                        </div>
+    fetchPosts();
+  }, []);
+  return (
+    <>
+      <Navbar />
+
+      <div className="flex  px-6">
+        <div className="border-r border-gray pt-8 px-6">
+          {" "}
+          {/*Sidebar*/}
+          <div>All posts</div>
+          {/* Just a fix for now */}
+          <div className="mr-4">Ingredient Glossary</div>
+        </div>
+        <div className="flex-1  px-12 mb-16">
+          {" "}
+          {/* Main Content 1. ... 2. ...*/}
+          <div className="flex justify-center flex-wrap gap-4">
+            {posts &&
+              posts?.map((post) => (
+                <div className="w-3/12" key={post.id}>
+                  <img
+                    className="h-auto max-w-full rounded-lg"
+                    src={post.imgUrl}
+                    alt={post.title}
+                  />
+                  <div className="flex items-center justify-between">
+                    <div>
+                      {post.title.slice(0, 20)}{" "}
+                      {post.title.length > 19 ? "..." : ""}
                     </div>
-
-
-
-
+                    <div>
+                      <Link href={`/single-page?postId=${post.id}`}>
+                        <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded my-2">
+                          View
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
                 </div>
+              ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-
-
-
-
-
-            </div>
-
-
-        </>
-    )
-}
-
-export default AllPost
+export default AllPost;
