88b0dd0cb3632b925420a9b28379e931e69231b0
Mocarram Hossain <mocarramhossain@gmail.com>
2023-04-17 06:51:55 +0600

    feat: img cdn's

--- dina-frontend/next.config.js
+++ dina-frontend/next.config.js
@@ -2,8 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['flowbite.s3.amazonaws.com'],
+    domains: [
+      "flowbite.s3.amazonaws.com",
+      "firebasestorage.googleapis.com",
+      "lh3.googleusercontent.com",
+    ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
